ede644156df1efeb17ba9fb3325444e87999bd13
Jonathan Melo <desenvolvedor.jonathan@gmail.com>
2023-05-19 16:25:48 +0100

    Setting Up material UI with Typescript and creating our custom theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { ThemeProvider } from '@emotion/react';
 import React, { FC, ReactElement } from 'react';
+import { customTheme } from './theme/customTheme';
+import { CssBaseline } from '@mui/material';
 
 const App: FC = (): ReactElement => {
-  return <h1>Hey, how you doing?! :)</h1>;
+  return (
+    <ThemeProvider theme={customTheme}>
+      <CssBaseline />
+      <h1>Hey, how you doing?! :)</h1>
+    </ThemeProvider>
+  );
 };
 
 export default App;
---
+++ src/theme/customTheme.ts
@@ -0,0 +1,7 @@
+import { createTheme, ThemeOptions } from '@mui/material';
+
+export const customTheme: ThemeOptions = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
