10cb2abeceebbf7adad5486a1f0b6e2f1c4b8a1e
Jonathan Melo <desenvolvedor.jonathan@gmail.com>
2023-05-19 18:51:22 +0100

    Setting up tanstack to make the API requests

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,19 @@ import React, { FC, ReactElement } from 'react';
 import { customTheme } from './theme/customTheme';
 import { CssBaseline } from '@mui/material';
 import { AppRoutes } from './routes';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
 
 const App: FC = (): ReactElement => {
   return (
-    <ThemeProvider theme={customTheme}>
-      <CssBaseline />
-      <AppRoutes />
-    </ThemeProvider>
+    <QueryClientProvider client={new QueryClient()}>
+      <ThemeProvider theme={customTheme}>
+        <CssBaseline />
+        <AppRoutes />
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 };
 
