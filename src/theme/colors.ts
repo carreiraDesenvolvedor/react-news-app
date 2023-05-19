fd84d89c9bcc5868d954e6c02a7ffb1064a70015
Jonathan Melo <desenvolvedor.jonathan@gmail.com>
2023-05-19 17:33:38 +0100

    Adding a few custom colors

---
+++ src/theme/colors.ts
@@ -0,0 +1,8 @@
+interface IColors {
+  [key: number]: string;
+}
+export const colorsNeutral: IColors = {
+  50: '#F8F9FA',
+  500: '#6C737F',
+  900: '#111927',
+};
--- src/theme/createPalette.ts
+++ src/theme/createPalette.ts
@@ -1,11 +1,17 @@
-import { PaletteOptions } from '@mui/material';
+import { PaletteOptions, alpha } from '@mui/material';
 import { common } from '@mui/material/colors';
+import { colorsNeutral } from './colors';
 
 export const createPalette = (): PaletteOptions => {
   return {
     background: {
       default: common.white,
       paper: common.white,
+    },
+    text: {
+      primary: colorsNeutral[900],
+      secondary: colorsNeutral[500],
+      disabled: alpha(colorsNeutral[900], 0.38),
     },
   };
 };
