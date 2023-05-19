19167e85538751a7b4f0358cf279f8b6d2c456a9
Jonathan Melo <desenvolvedor.jonathan@gmail.com>
2023-05-19 17:31:11 +0100

    Frontend - Creating the AuthHeader component

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -1,9 +1,11 @@
 import React, { FC, ReactElement } from 'react';
 import { AuthLayout } from '../../layouts/auth/';
+import { AuthHeader } from './_auth-header';
 
 export const LoginPage: FC = (): ReactElement => {
   return (
     <AuthLayout>
+      <AuthHeader />
       <h1>Login Page Here</h1>
     </AuthLayout>
   );
---
+++ src/pages/auth/_auth-header.tsx
@@ -0,0 +1,16 @@
+import { Link, Stack, Typography } from '@mui/material';
+import React, { FC, ReactElement } from 'react';
+
+export const AuthHeader: FC = (): ReactElement => {
+  return (
+    <Stack spacing={1} sx={{ mb: 3 }}>
+      <Typography variant="h4">Login</Typography>
+      <Typography color="text.secondary" variant="body2">
+        Don&apos;t have an account?{' '}
+        <Link underline="hover" variant="subtitle2">
+          Register
+        </Link>
+      </Typography>
+    </Stack>
+  );
+};
