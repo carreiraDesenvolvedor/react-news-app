82c6bbd4beae2172b69ce0cc57fca3fb71d4267e
Jonathan Melo <desenvolvedor.jonathan@gmail.com>
2023-05-19 17:32:23 +0100

    Adding a few more custom Typography and removing the dark mode

--- src/theme/createPalette.ts
+++ src/theme/createPalette.ts
@@ -3,7 +3,6 @@ import { common } from '@mui/material/colors';
 
 export const createPalette = (): PaletteOptions => {
   return {
-    mode: 'dark',
     background: {
       default: common.white,
       paper: common.white,
--- src/theme/createTypography.ts
+++ src/theme/createTypography.ts
@@ -10,8 +10,24 @@ export const createTypography = (): TypographyOptions => {
       fontSize: '3.5rem',
       lineHeight: 1.2,
     },
+    h4: {
+      fontFamily: "'Plus Jakarta Sans', sans-serif",
+      fontWeight: 700,
+      fontSize: '2rem',
+      lineHeight: 1.2,
+    },
     subtitle1: {
       fontSize: '1rem',
+      fontWeight: 500,
+      lineHeight: 1.57,
+    },
+    body2: {
+      fontSize: '0.875rem',
+      fontWeight: 400,
+      lineHeight: 1.57,
+    },
+    subtitle2: {
+      fontSize: '0.875rem',
       fontWeight: 500,
       lineHeight: 1.57,
     },
