7e0b8eb3b01d63a8c40c89e91de43b1837007314
Jonathan Melo <desenvolvedor.jonathan@gmail.com>
2023-05-19 16:38:32 +0100

    Finishing the implementation of our AuthLayoutRightColumn.tsx

--- src/layouts/auth/_right-column.tsx
+++ src/layouts/auth/_right-column.tsx
@@ -1,16 +1,57 @@
-import { Grid } from '@mui/material';
+import { Box, Grid, Typography } from '@mui/material';
 import React, { FC, ReactElement } from 'react';
 
 export const AuthLayoutRightColumn: FC =
   (): ReactElement => {
     return (
       <Grid
-        sx={{ background: 'yellow' }}
         item
         xs={12}
         lg={6}
+        display={'flex'}
+        justifyContent={'center'}
+        alignItems={'center'}
+        height={'100%'}
+        sx={{
+          color: 'white',
+          background:
+            'radial-gradient(50% 50% at 50% 50%, #122647 0%, #090E23 100%)',
+          '& img': {
+            width: '100%',
+            maxWidth: '600px',
+          },
+        }}
       >
-        Fixed Content
+        <Box sx={{ p: 3 }}>
+          <Typography
+            align="center"
+            color="inherit"
+            sx={{
+              fontSize: '24px',
+              lineHeight: '32px',
+              mb: 1,
+            }}
+            variant="h1"
+          >
+            Welcome to{' '}
+            <Box
+              component="a"
+              sx={{ color: '#15B79E' }}
+              target="_blank"
+            >
+              NewsFeed
+            </Box>
+          </Typography>
+          <Typography
+            align="center"
+            sx={{ mb: 3 }}
+            variant="subtitle1"
+          >
+            A professional App to help you follow the most
+            important News for you!
+          </Typography>
+          <img alt="" src="/assets/auth/news-art.png" />
+        </Box>
       </Grid>
     );
   };
